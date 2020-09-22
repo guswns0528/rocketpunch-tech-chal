9af1543c8711c1b5dcb57fe70d4795fcba421021
Joon <guswns0528@gmail.com>
2020-09-22 20:26:21 +0900

    로그인, 채팅 리스트, 채팅 불러오기 구현

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+import simplejson
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class ChatRoom(models.Model):
+
+    def to_dict(self):
+        return {'room_id': self.pk}
+    
+
+class Participant(models.Model):
+    room = models.ForeignKey(ChatRoom, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    last_read = models.ForeignKey('Message', on_delete=models.DO_NOTHING, null=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['user']),
+            models.Index(fields=['user', 'room'])
+        ]
+
+        unique_together = [
+            ['room', 'user']
+        ]
+
+    def to_dict(self):
+        return {'room_id': self.room_id}
+
+
+class Message(models.Model):
+    room = models.ForeignKey(ChatRoom, on_delete=models.DO_NOTHING)
+    sender = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    content = models.TextField()
+    created_at = models.DateTimeField()
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['room', 'created_at'])
+        ]
+
+    def to_dict(self, prev_readed=None):
+        return {
+            'msg_id': self.pk,
+            'sender': self.sender.username,
+            'content': self.content,
+            'created_at': self.created_at,
+            'readed': prev_readed and self.pk <= prev_readed or False
+        }
+
--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,3 +1,90 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from django.contrib.auth import authenticate, login, logout
+from .models import ChatRoom
 
-# Create your views here.
+
+def login(request, username, password):
+    user = authenticate(username=username, password=password):
+    if user is not None:
+        login(request, user)
+        return JsonResponse({'msg': 'ok'})
+    else:
+        return JsonResponse({
+            'msg': 'User dose not exist.',
+            status=404)
+
+
+def login_required(f):
+    import functools
+
+    @functools.wraps(f)
+    def wrapper(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return f(request, *args, **kwargs)
+        else:
+            return JsonResponse({
+                'msg': 'Login needed.'
+                status=400})
+    return wrapper
+
+
+@login_required
+def logout(request):
+    logout(request)
+    return JsonResponse({'msg': 'ok'})
+
+
+@login_required
+def create_chatroom(request, other_user):
+    # channel_layer = get_channel_layer()
+    # async_to_sync(channel_layer.group_add(~~~))
+    pass
+
+
+@login_required
+def list_chatrooms(request):
+    user = request.user
+    rooms = Participant.objects.filter(user=user)
+    return JsonResponse({'rooms': rooms})
+
+
+@login_required
+def get_last_messages(request, chatroom_id):
+    user = request.user
+    participant = get_object_or_404(
+        Participant.objects.filter(user=user, room_id=chatroom_id)
+    )
+    messages = Message.objects.select_related('sender')
+        .filter(room_id=chatroom_id)
+        .order_by('created_at')[-50:0]
+    return JsonResponse(
+        [message.to_dict(prev_readed=participant.last_read_id)
+            for message in messages]
+    )
+
+
+@login_required
+def get_messages_since(request, chatroom_id, since):
+    user = request.user
+    participant = get_object_or_404(
+        Participant.objects.filter(user=user, room_id=chatroom_id)
+    )
+    messages = Message.objects.select_related('sender')
+        .filter(room_id=chatroom_id)
+        .filter(pk__gt=since)
+        .order_by('created_at')[-50:0]
+    return JsonResponse(
+        [message.to_dict(prev_readed=participant.last_read_id)
+            for message in messages]
+    )
+
+
+@login_required
+def send_message(request, chatroom_id, content):
+    import datetime
+
+    new_message = Message.objects.create(room_id=chatroom_id,
+        user=request.user,
+        content=content,
+        created_at=datetime.now())

01a2825d33e2c75be0c178d71109d97d5d722c99
Joon <guswns0528@gmail.com>
2020-09-23 08:10:26 +0900

    Register model to django admin page.

--- backend/chat/admin.py
+++ backend/chat/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import ChatRoom, Participant, Message
 
-# Register your models here.
+
+class ChatRoomAdmin(admin.ModelAdmin):
+    pass
+
+
+class ParticipantAdmin(admin.ModelAdmin):
+    pass
+
+
+class MessageAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(ChatRoom, ChatRoomAdmin)
+admin.site.register(Participant, ParticipantAdmin)
+admin.site.register(Message, MessageAdmin)

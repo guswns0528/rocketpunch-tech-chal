7a97e3a9c004bb5a6a5ac0a8ca728d131a18130e
Joon <guswns0528@gmail.com>
2020-09-24 10:29:50 +0900

    [frontend] ChatRoom component구현.

---
+++ frontend/src/components/ChatRoom.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="chatroom-wrapper">
+    <div class="chatroom-view">
+      <div v-if="isValid" class="chatroom-messages">
+        <div v-for="message in room.messages" v-bind:key="message.messageId" class="message">
+          <span class="message-sender">{{message.sender}}</span>
+          <p class="message-content">
+            {{message.content}}
+          </p>
+          <span class="message-time">{{dateToString(message.createdAt)}}</span>
+        </div>
+      </div>
+    </div>
+    <div class="chatroom-input">
+      <form v-on:submit.prevent="sendChat">
+        <textarea v-model=chatInput placeholder="내용을 작성해주세요."></textarea>
+        <div>
+          <input class="button" type="submit" value="보내기">
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.chatroom-wrapper {
+  width: 70%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  padding-left: 10%;
+  padding-right: 50px;
+}
+
+.chatroom-view {
+  width: 100%;
+  height: 80%;
+}
+
+.chatroom-messages {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  overflow: auto;
+}
+
+.chatroom-input {
+  width: 100%;
+  height: 150px;
+  padding-top: 30px;
+}
+
+.message {
+  display: flex;
+  flex-direction: column;
+  color: #555555;
+  margin: 10px 0;
+}
+
+.message-sender {
+  color: #000000;
+}
+
+.message-time {
+  color: #888888;
+}
+
+.button {
+  background-color: #5b79ff;
+  border: none;
+  color: white;
+  text-align: center;
+  display: inline-block;
+  font-size: 16px;
+  height: 30px;
+  padding: 0 30px;
+  border-radius: 15px;
+}
+
+
+</style>
+
+<script lang="ts">
+import Vue from 'vue';
+
+import {Room} from '../data';
+import {dateToPresentationString} from '../util/date';
+
+export default Vue.extend({
+  data() {
+    return {
+      chatInput: ''
+    }
+  },
+
+  props: {
+    room: {
+      type: Object as () => Room | undefined,
+    }
+  },
+
+  computed: {
+    isValid() {
+      return this.room !== undefined;
+    }
+  },
+
+  methods: {
+    sendChat(ev: Event) {
+      console.log(this.chatInput);
+      console.log(this.room);
+      this.chatInput = '';
+      return;
+    },
+
+    dateToString(date: Date): string {
+      // FIXME: this is not reactive.
+      return dateToPresentationString(date, new Date());
+    }
+  },
+
+  watch: {
+    room(_newVal, _oldVal) {
+      this.chatInput = '';
+    }
+  }
+});
+</script>

f6357e9eea653a28f70da358596e2311ee74a5ba
Joon <guswns0528@gmail.com>
2020-09-24 10:31:44 +0900

    [frontend] register routings.

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,9 +1,53 @@
 import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
+import VueRouter, { RouteConfig, NavigationGuard } from 'vue-router'
+
+import Login from '../views/Login.vue'
+import Chat from '../views/Chat.vue'
+
+import {authCheck} from './utils'
 
 Vue.use(VueRouter)
 
+const requireAuth: NavigationGuard = (to, from, next) => {
+  if (authCheck()) {
+    next();
+  }
+  else {
+    next({
+      path: '/login'
+    });
+  }
+}
+
+const notLogined: NavigationGuard = (to, from, next) => {
+  if (!authCheck()) {
+    next();
+  }
+  else {
+    next({
+      path: '/'
+    });
+  }
+}
+
 const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    component: Chat,
+    beforeEnter: requireAuth
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+    beforeEnter: notLogined
+  },
+  {
+    path: '/chat/:id',
+    name: 'chat',
+    component: Chat,
+    beforeEnter: requireAuth
+  }
 ]
 
 const router = new VueRouter({

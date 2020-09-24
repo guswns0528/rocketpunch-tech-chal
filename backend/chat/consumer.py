a823438710a64ff66b915d03b92a5f3116c3c76d
Joon <guswns0528@gmail.com>
2020-09-24 11:43:49 +0900

    [backend] remove ws consumer auth middlewear.

--- backend/chat/consumer.py
+++ backend/chat/consumer.py
@@ -1,8 +1,24 @@
-import simplejson
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
-from .models import Participant, Message, ChatRoom, Connection
+from .models import Participant, Message, ChatRoom, Connection, User
+
+from backend.settings import SECRET_KEY
+
+import simplejson
 import asyncio
+import jwt
+
+
+@database_sync_to_async
+def get_user(api_token):
+    try:
+        payload = jwt.decode(api_token, SECRET_KEY, algorithm='HS256')
+        user = User.objects.get(pk=payload['user_id'])
+    except jwt.exceptions.DecodeError:
+        return None
+    except User.DoesNotExist:
+        return None
+    return user
 
 
 @database_sync_to_async
@@ -39,29 +55,13 @@ def delete_connection(connection_id):
 class ChatConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
-        user = self.scope['user']
-        if user.is_anonymous:
-            await self.close()
-            return
-
-        connection = await create_connection(user, self.channel_name)
-        self.participated_rooms = await get_participated(user)
-        self.connection_id = connection.pk
-
-        room_names = [
-            ChatRoom.room_id_to_room_name(room)
-            for room in self.participated_rooms
-        ]
-        await asyncio.wait([
-            self.channel_layer.group_add(room_name, self.channel_name)
-            for room_name in room_names
-        ])
-
+        # just accept
+        self.user = None
         await self.accept()
 
     async def disconnect(self, code):
-        user = self.scope['user']
-        if user.is_anonymous:
+        user = self.user
+        if user is None:
             return
 
         # NOTE: sometimes, disconnect fail to run.
@@ -75,12 +75,41 @@ class ChatConsumer(AsyncWebsocketConsumer):
             for room_name in room_names
         ])
 
-    async def receive(self, data):
+    async def receive(self, text_data=None, bytes_data=None):
         # NOTE: Do I need to handle leaving a chat room?
-        data = simplejson.loads(data)
-        room_id = data['roomId']
+        data = simplejson.loads(text_data)
+        message_type = data.get('type', None)
+        if message_type == 'auth':
+            user = await get_user(data['apiToken'])
+            if user is None:
+                await self.close()
+                return
+
+            self.user = user
+            connection = await create_connection(user, self.channel_name)
+            self.participated_rooms = await get_participated(user)
+            self.connection_id = connection.pk
+
+            room_names = [
+                ChatRoom.room_id_to_room_name(room)
+                for room in self.participated_rooms
+            ]
+            await asyncio.wait([
+                self.channel_layer.group_add(room_name, self.channel_name)
+                for room_name in room_names
+            ])
+            return
+        elif self.user is None:
+            await self.close()
+
+        user = self.user
+        room_id = data.get('roomId', None)
+        if room_id is None:
+            return
         # TODO: sanitize content.
-        content = data['content']
+        content = data.get('content', None)
+        if content is None:
+            return
 
         new_message = await store_message(user, room_id, content)
 
@@ -89,17 +118,23 @@ class ChatConsumer(AsyncWebsocketConsumer):
             {
                 'type': 'handle_message',
                 'message': new_message.to_dict(),
+                'roomId': new_message.room_id
             }
         )
 
     async def handle_message(self, event):
+        if self.user is None:
+            return
         new_message = event['message']
         await self.send(simplejson.dumps({
             'type': 'MSG',
-            **new_message
+            'roomId': event['roomId'],
+            'message': {**new_message}
         }))
 
     async def join(self, event):
+        if self.user is None:
+            return
         join_msg = {
             'type': 'JOIN',
             'roomId': event['roomId'],
--- backend/chat/routing.py
+++ backend/chat/routing.py
@@ -2,10 +2,7 @@ from channels.routing import URLRouter
 from django.urls import path
 
 from . import consumer
-from .util import TokenAuthMiddlewareStack
 
-patterns = TokenAuthMiddlewareStack(
-    URLRouter([
-        path(r'ws/chat/', consumer.ChatConsumer),
-    ])
-)
+patterns = URLRouter([
+    path(r'ws/chat/', consumer.ChatConsumer),
+])
